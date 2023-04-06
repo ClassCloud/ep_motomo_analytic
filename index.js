c467c2d8534d95c0e654e53b4f95f6ae407b08dd
Patric Lougheed <xcentrikmedia@gmail.com>
2023-04-06 11:09:36 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,28 +1,34 @@
 var eejs = require('ep_etherpad-lite/node/eejs/');
 var settings = require('ep_etherpad-lite/node/utils/Settings');
 
-if (settings.ep_google_analytics){ 
-  var gaCode = settings.ep_google_analytics.gaCode;
+if (settings.ep_matomo_analytics){ 
+  var idCode = settings.ep_matomo_analytics.idCode;
 }else{
-  var gaCode = false;
+  var idCode = false;
 }
 
 exports.eejsBlock_scripts = function (hookName, context, cb) {
-  if (gaCode) {
+  if (idCode) {
     var gaString = `
-      <!-- Google tag (gtag.js) -->
-      <script async src="https://www.googletagmanager.com/gtag/js?id=${gaCode}"></script>
-      <script>
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-
-        gtag('config', '${gaCode}');
-      </script>
+    <!-- Matomo -->
+<script>
+  var _paq = window._paq = window._paq || [];
+  /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+  _paq.push(['trackPageView']);
+  _paq.push(['enableLinkTracking']);
+  (function() {
+    var u="https://analytics.tangibility.ca/";
+    _paq.push(['setTrackerUrl', u+'matomo.php']);
+    _paq.push(['setSiteId', '{idCode}']);
+    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+    g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+  })();
+</script>
+<!-- End Matomo Code -->
   `
     context.content = gaString + context.content;
   } else {
-    console.log("Google Analytics code not set.")
+    console.log("MAtomo Analytics code not set.")
   }
   
   return cb();
